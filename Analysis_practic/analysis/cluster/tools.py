e8bf113fb88950836c7d44ad694f6fe4888cd5a1
2Bee <twobl4des@gmail.com>
2016-07-17 19:24:22 +0700

    Views

--- Analysis_practic/analysis/cluster/tools.py
+++ Analysis_practic/analysis/cluster/tools.py
@@ -1,21 +1,77 @@
+#coding: utf8
 import math
+#from cluster.models import Student, Discipline, Scores
+
+
+class Cluster:
+    def __init__(self):
+        self.centroid = None
+        self.prev_centroid = None
+        self.cluster_coordinates = None
+        self.objects = {}
+
+    def set_centroid(self, new_centroid):
+        self.prev_centroid = list(new_centroid) if self.prev_centroid == None else self.centroid
+        self.centroid = list(new_centroid)
+
+    def add_object(self, key, value):
+        self.objects.update({key: value})
+
+    def recalculate_centroid(self):
+        coordinate_lists = zip(*(self.objects.values()))  # лист листов 1х, 2х и т.д. координат
+        new_centroid_coordinates = map(lambda x: sum(x)/len(x), coordinate_lists)
+        #self.prev_centroid = self.centroid
+        self.__calculate_cluster_coordinates()  # координаты кластера для вывода на график
+        self.set_centroid(new_centroid_coordinates)  # для вывода нужно преобразовать в list
+
+    def clear_cluster(self):
+        self.objects.clear()
+
+    def __calculate_cluster_coordinates(self):
+        coordinate_lists = zip(*(self.objects.values()))  # лист листов 1х, 2х и т.д. координат
+        self.cluster_coordinates = list(map(lambda x: sum(x)/len(x)*100, coordinate_lists))
 
 
 def euclidian_distance(score1, score2):
-    sum_sqr = 0
-    for i, j in zip(score1, score2):
-        sum_sqr += (int(i)-int(j))**2
-    distance = math.sqrt(sum_sqr)
+    distance = math.sqrt(sum([(x-y)**2 for x, y in zip(score1, score2)]))
+    return float("%.15f" % distance)
 
-    return distance
 
-def new_centroid(scores):
-    new_centroid_coordinates = []
-    devisor = len(scores)
-    average_score = 0
-    for j in range(len(scores[0]) - 1):
-        for i in scores:
-            average_score =+ scores[i][j]/devisor
-        new_centroid_coordinates.append(average_score)
-        average_score = 0
-    return new_centroid_coordinates
+def clustering(scores, __clusters):
+    for score in scores:
+        distances = [euclidian_distance(scores[score], cluster.centroid) for cluster in __clusters]  # лист с расстояниями до каждого центроида
+        clnum = distances.index(min(distances))  # определение наименьшего расстояния и соответственно кластера, в который будет помещен объект
+        __clusters[clnum].add_object(score, scores[score])  # запись в кластер
+
+
+def k_means(__data, __clusters):
+    for num, cluster in enumerate(__clusters, 0):
+        cluster.set_centroid(__data[list(__data.keys())[num]])
+    clustering(__data, __clusters)
+    #for cluster in clusters:
+    #    cluster.recalculate_centroid()
+    [cluster.recalculate_centroid() for cluster in __clusters]  # То же самое, что и две предыдущих строки
+    while any(abs(euclidian_distance(cluster.centroid, cluster.prev_centroid)) > 10**-3 for cluster in __clusters):  # С точностью надо будет определиться еще
+        #for cluster in clusters:
+        #    cluster.clear_cluster()
+        [cluster.clear_cluster() for cluster in __clusters]
+        clustering(__data, __clusters)
+        #for cluster in __clusters:
+        #    cluster.recalculate_centroid()
+        [cluster.recalculate_centroid() for cluster in __clusters]
+
+
+
+#data = {int(str(id)): [int(str(s)) for s in Scores.objects.filter(student_id=id)] for id in Student.objects.all()}
+data = {1: [5, 4], 2: [5, 3], 4: [4, 4], 5: [3, 4]}
+
+cluster1 = Cluster()
+cluster2 = Cluster()
+cluster3 = Cluster()
+clusters = [cluster1, cluster2, cluster3]
+
+k_means(data, clusters)
+cluster3
+
+#print(cluster2.centroid, cluster2.objects, cluster2.cluster_coordinates)
+
--- Analysis_practic/analysis/cluster/views.py
+++ Analysis_practic/analysis/cluster/views.py
@@ -1,21 +1,39 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader, Template, Context
 from cluster.models import Student, Discipline, Scores
-from cluster.tools import new_centroid
+from django.http import Http404
+#from cluster.tools import new_centroid
 
 def index(request):
+    template = loader.get_template('index2.html')
+    data1 = list(Student.objects.filter(pk=1002))
+    data2 = list(Student.objects.filter(pk=1003))
+    data3 = list(Student.objects.filter(pk=1004))
+    clusters = [data1, data2, data3]
+    # {int(str(id)): [int(str(s)) for s in Scores.objects.filter(student_id=id)] for id in Student.objects.all()}
+    #data_key = data.keys()
+    #data_values = data.values()
+    #centroids = [data_values[0], data_values[1], data_values[2]]
+    #new_centroids = []
+    #clusters = []
+    #for i in clusters:
+    #    new_centroids.append = new_centroid(clusters[i])
+    #centroids = new_centroids
+    #new_centroids = []
+    context = RequestContext(request, {
+        'clusters': clusters,
+    })
+    return HttpResponse(template.render(context))
 
-    data = {int(str(id)): [int(str(s)) for s in Scores.objects.filter(student_id=id)] for id in Student.objects.all()}
-    data_key = data.keys()
-    data_values = data.values()
-    centroids = [data_values[0], data_values[1], data_values[2]]
-    new_centroids = []
-    clusters = []
-    for i in clusters:
-        new_centroids.append = new_centroid(clusters[i])
-    centroids = new_centroids
-    new_centroids = []
 
-
-    return HttpResponse(data)
+def detail(request, object_id):
+    try:
+        object = Student.objects.get(pk=object_id)
+        object_scores = Scores.objects.filter(student=object_id).order_by('-discipline')
+    except Student.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'detail.html', {'object': object,
+                                           'object_scores': object_scores})
 # Create your views here.
